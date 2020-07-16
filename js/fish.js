6e9faaa95e6e55aa3e7c88cc6f567fd3044ee76b
Kris Sum <kris@switchsystems.co.uk>
2020-07-16 11:41:22 +0100

    Cleaned up styles, added link to survey

--- js/fish.js
+++ js/fish.js
@@ -40,7 +40,7 @@ var fishConfig = {
         },
     ],
     imagePath: '/images/fish',
-    tiltAngleThreshold: 10,
+    tiltAngleThreshold: 15,
 };
 
 class Fish {
@@ -64,9 +64,12 @@ class Fish {
 
             var listItem = template.content.cloneNode(true);
             listItem.querySelectorAll('td')[0].innerHTML = animation.name;
-            listItem.querySelectorAll('td')[1].innerHTML = animation.description;
+            listItem.querySelectorAll('td')[2].innerHTML = animation.description;
 
             if (this.choice == x) {
+
+                listItem.querySelectorAll('td')[1].innerHTML = '<svg aria-hidden="true" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-double-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">                <path fill-rule="evenodd" d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>                <path fill-rule="evenodd" d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>              </svg>';
+
                 listItem.querySelector('tr').classList.add('table-active');
                 if (listItem.childNodes.length > 0) { 
                     elem.insertBefore(listItem, elem.childNodes[0]);
@@ -78,6 +81,7 @@ class Fish {
                 var img = document.createElement('img');
                 img.classList.add('img-fluid');
                 img.src = this.fishConfig.imagePath + '/' + animation.file;
+                img.alt = "Image of fish movement - " + animation.name;
                 document.getElementById(this.readingResult).appendChild(img);
 
             } else { 
